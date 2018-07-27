c4e2d54104d0091484bc1c9f9de386db03df8022
Karl Martin Teras <karlnitram@gmail.com>
2018-07-28 01:24:21 +0300

    Updated list of file names to become list of chat names

--- routes/index.js
+++ routes/index.js
@@ -7,23 +7,49 @@ const messagesFolder = 'messages';
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  fs.readdir(messagesFolder, (err, files) => {
-    files.forEach(file => {
-      readChat(file)
+    fs.readdir(messagesFolder, (err, fileNames) => {
+        renderChatNamesList(res, fileNames);
     });
-    res.render('index', { title: 'Express', files: files});
-  });
 });
 
 router.get('/chat/:chatName', (req, res) => {
-  console.log(req.params);
-  res.render('chat', { test: req.params['chatName'] });
+    console.log(req.params);
+    res.render('chat', {test: req.params['chatName']});
 });
 
+function renderChatNamesList(res, fileNames) {
+    getChatNames(fileNames, (chatNames) => {
+        res.render('index', {title: 'Chats', files: chatNames});
+    });
+}
+
+function getChatNames(fileNames, callback) {
+    let chatNames = [];
+
+    function addChatName(i) {
+        if(i < fileNames.length) {
+            fs.readFile(path.join(messagesFolder, fileNames[i], 'message.json'), 'utf-8', (err, fileData) => {
+                if (err === null) {
+                    let chatJson = JSON.parse(fileData);
+                    chatNames.push(chatJson['title']);
+                    console.log(chatJson['title'], i);
+                }
+                addChatName(i + 1);
+            });
+        }
+        else {
+            // TODO: cache chat list?
+            callback(chatNames);
+        }
+    }
+
+    addChatName(0);
+}
+
 function readChat(folder) {
-  fs.readFile(path.join(messagesFolder, folder, 'message.json'), 'utf-8', (err, file) => {
-    console.log(file);
-  });
+    fs.readFile(path.join(messagesFolder, folder, 'message.json'), 'utf-8', (err, file) => {
+        console.log(file);
+    });
 }
 
 module.exports = router;
