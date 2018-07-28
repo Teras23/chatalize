2ee751e66520925b87b5ed854eb288a7cf0ab555
Teras23 <karl.martin@ter.as>
2018-07-29 01:12:33 +0300

    Sorted main list by message count

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,29 @@
 let express = require('express');
 let fs = require('fs');
 let path = require('path');
+let stripBom = require('strip-bom');
 let router = express.Router();
 
 const messagesFolder = 'messages';
 
+let chatNames = undefined;
+let chatData;
 let chatToFileNames = [];
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
     fs.readdir(messagesFolder, (err, fileNames) => {
-        renderChatNamesList(res, fileNames);
+        updateChatData(fileNames, (chatData) => {
+            let infoList = chatDataToList(chatData);
+
+            infoList.sort((first, second) => {
+                return first.messageCount < second.messageCount ? 1 : first.messageCount > second.messageCount ? -1 : 0;
+            });
+
+            res.render('index', {title: "Chats", files: infoList});
+        });
+
+        // renderChatNamesList(res, fileNames);
     });
 });
 
@@ -23,8 +36,8 @@ router.get('/chat/:chatName', (req, res) => {
     readChat(chatToFileNames[chatName], (err, fileData) => {
         let chatJson = JSON.parse(fileData);
         let messageCount = getMessageCount(chatJson);
-        console.log(chatToFileNames);
         console.log(messageCount);
+        res.setEncoding('utf16');
         res.render('chat', {chatName: chatName, messageCount: messageCount});
     });
 });
@@ -35,31 +48,69 @@ function renderChatNamesList(res, fileNames) {
     });
 }
 
-function getChatNames(fileNames, callback) {
-    let chatNames = [];
+function updateChatData(fileNames, callback) {
+    chatData = [];
 
-    function addChatName(i) {
-        if(i < fileNames.length) {
+    function updateData(i) {
+        if (i < fileNames.length) {
             readChat(fileNames[i], (err, fileData) => {
                 if (err === null) {
-                    let chatJson = JSON.parse(fileData);
-                    chatNames.push(chatJson['title']);
-                    chatToFileNames[chatJson['title']] = fileNames[i];
+                    chatData[fileNames[i]] = JSON.parse(fileData);
                 }
-                addChatName(i + 1);
+                updateData(i + 1);
             });
         }
         else {
-            // TODO: cache chat list?
-            callback(chatNames);
+            callback(chatData);
         }
     }
 
-    addChatName(0);
+    updateData(0);
+}
+
+function getChatNames(fileNames, callback) {
+    if (chatNames === undefined) {
+
+        chatNames = [];
+
+        function addChatName(i) {
+            if (i < fileNames.length) {
+                readChat(fileNames[i], (err, fileData) => {
+                    if (err === null) {
+                        let chatJson = JSON.parse(fileData);
+                        chatNames.push(chatJson['title']);
+                        chatToFileNames[chatJson['title']] = fileNames[i];
+                    }
+                    addChatName(i + 1);
+                });
+            }
+            else {
+                // TODO: cache chat list?
+                callback(chatNames);
+            }
+        }
+
+        addChatName(0);
+    }
+    else {
+        callback(chatNames);
+    }
+}
+
+function chatDataToList(cd) {
+    let list = [];
+    for (let key in cd) {
+        list.push({
+            title: cd[key]['title'],
+            messageCount: cd[key]['messages'].length
+        });
+    }
+    return list;
 }
 
 function readChat(chatName, callback) {
-    fs.readFile(path.join(messagesFolder, chatName, 'message.json'), 'utf-8', (err, fileData) => {
+    let filePath = path.join(messagesFolder, chatName, 'message.json');
+    fs.readFile(filePath, 'utf-8', (err, fileData) => {
         callback(err, fileData);
     });
 }
